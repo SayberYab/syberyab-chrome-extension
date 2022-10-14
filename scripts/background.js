6af51abfad5b68507a4d9dee2a740840a4d559c1
kiumad <mirfatemi7@gmail.com>
2022-10-14 19:35:52 +0330

    ...

--- scripts/background.js
+++ scripts/background.js
@@ -1,31 +1,10 @@
-// setTimeout(() => {
-//   //   changeinContent();
-//   chrome.runtime.sendMessage({
-//     msg: "something_completed",
-//     data: {
-//       subject: "Loading",
-//       content: "Just completed!",
-//     },
-//   });
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.scripting.executeScript({
+    target: { tabId: tabs[0].id },
+    files: ["content.js"],
+  });
+});
 
-//   chrome.runtime.onMessage.addListener(function (msg) {
-//     console.log("message recieved" + msg);
-//   });
-// }, 5000);
-
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   console.log("message recived", request);
-// });
-
-// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//   chrome.tabs.sendMessage(
-//     tabs[0].id,
-//     { greeting: "hello" },
-//     function (response) {
-//       console.log(response);
-//     }
-//   );
-// });
 var cyber_status = "true";
 chrome.storage.sync.get(["cyber-status"], function (result) {
   if (result["cyber-status"]) cyber_status = result["cyber-status"];
